df00ea43c3aaa230a2092d0e506802eabca3289e
PoBlue <250299430@qq.com>
2017-11-13 21:08:16 +0800

    [utils] export csv

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import json
+import csv
 
 # save json data to file
 def save_data_to_path(path, data):
@@ -10,3 +11,17 @@ def get_data_from_path(path):
     with open(path) as json_file:  
         data = json.load(json_file)
         return data
+
+
+def export_csv(_headers, _rows, _path):
+    """
+    _rows: [ 
+        (a,b,c), 
+        (f,g,h), 
+    ]
+    _header: [a, b, c]
+    """
+    with open(_path, 'w') as f:
+        f_csv = csv.writer(f)
+        f_csv.writerow(_headers)
+        f_csv.writerows(_rows)
